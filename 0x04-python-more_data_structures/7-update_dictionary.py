dee0f6164d4b108fa46b30398e2ee143249351b4
aswani2r <ruudaswani26@gmail.com>
2023-10-16 00:28:58 -0700

    alx projects

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-my_dictionary = {'a': 1, 'b': 2}
-update_dictionary(my_dictionary, 'c', 3)
-update_dictionary(my_dictionary, 'a', 5)
+    return a_dictionary
