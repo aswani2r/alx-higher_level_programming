67ccfc3bd8720fa7de44c950792cce0c2c2761c7
aswani2r <ruudaswani26@gmail.com>
2023-10-16 00:06:40 -0700

    alx projects

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    total = 0
+
+    for item in my_list:
+        if item not in unique_integers:
+            unique_integers.add(item)
+            total += item
+
+    return total
