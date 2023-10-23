4db1771d4656433e0998b6d111c038f5217d69d4
aswani2r <ruudaswani26@gmail.com>
2023-10-23 00:51:38 -0700

    Alx projects

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            print(i, end=' ')
+            count += 1
+            if count == x:
+                break
+        except:
+            print("Error: Invalid element in the list")
+    print()
+    return count
