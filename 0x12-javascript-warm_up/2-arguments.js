1b998620608677f6e65800de8f2817c27a388914
aswani2r <ruudaswani26@gmail.com>
2024-01-18 01:25:31 -0800

    introduction to JavaScript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const numberOfArgurment = process.argv.length -2;
+if (numberOfArguments === 0 ){
+	console.log("No Argument");
+}else if (numberOfArguments == 1){
+	console.log("Argument found");
+}else{
+	console.log("Argument found");
+}
