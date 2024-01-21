6dad2679a96472092cc8c55193f029f9adefb8a0
aswani2r <ruudaswani26@gmail.com>
2024-01-20 21:45:28 -0800

    JavaScript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@ const parsedInt = parseInt(arg1, 10);
 if (isNaN(parsedInt)) {
 	console.log('Not a number');
 }else{
-	console.log('My number: $`{parsedInt}`);
+	console.log(`My number: ${parsedInt}`);
 }
