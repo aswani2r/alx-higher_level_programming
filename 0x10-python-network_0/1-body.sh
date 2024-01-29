39e39246f453e6424077797163b375c68c7f0f44
aswani2r <ruudaswani26@gmail.com>
2024-01-29 07:25:29 -0800

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and sends get request to the URL
-http_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); if [ "$http_code" = "200" ]; then curl -s "$1"; fi
+http_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$http_code" = "200" ] && curl -s "$1"
