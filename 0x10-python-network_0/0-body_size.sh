a9389796bbf6bf2388b6a87740084211f4aad7f1
aswani2r <ruudaswani26@gmail.com>
2024-01-29 06:07:33 -0800

    python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# this script takes URL and sends request to display the size in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
+# this script takesi URL and sends request to display the size in bytes
+curl -s "$1" --write-out "size: %{size_download}\n" -o /dev/null
