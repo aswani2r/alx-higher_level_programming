62eba4672426de0ffc97c3ffc2be68080bb3070f
aswani2r <ruudaswani26@gmail.com>
2024-02-22 01:16:23 -0800

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filepath = process.argv[2];
-fs.readFile(filepath, 'utf8', (err, data) =?> {
+fs.readFile(filepath, 'utf8', (err, data) => {
 	if (err) {
 		console.error(err);
 	} else {
